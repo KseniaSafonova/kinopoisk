206c6e8a56e4d5c9157460d391ad588fdd06327b
ksenias <kseniatsn@gmail.com>
2023-02-19 18:55:23 +0300

    description

--- kinopoisk/src/components/MoviePage.jsx
+++ kinopoisk/src/components/MoviePage.jsx
@@ -1,35 +1,34 @@
 import styles from './../styles/MoviePage.module.css'
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom';
 
 function MoviePage({ movies }) {
 
-    console.log(movies)
-
     let { id } = useParams();
+
+    const currentMovie = movies.filter((m) => m.kinopoiskId == id);
+
     // const [year, setYear] = useState('');
     // const [genres, setGenres] = useState([]);
     // const [backgroundImage, setBackgroundImage] = useState('');
     // const [description, setDescription] = useState('');
 
-    useEffect(() => {
-        const currentMovie = movies.filter((n) => n.id == id);
-        console.log(currentMovie)
-        // setYear(currentCard[0].year)
-        // setGenres(currentCard[0].genres)
-        // setBackgroundImage(currentCard[0].coverUrl)
-        // setDescription(currentCard[0].description)
-    }, [id])
+    // useEffect(() => {
+    //     setYear(currentMovie[0].year)
+    //     setGenres(currentMovie[0].genres)
+    //     setBackgroundImage(currentMovie[0].coverUrl)
+    //     setDescription(currentMovie[0].description)
+    // }, [id])
     return (
         <div className={styles.container}>
-            {/* <div>{year}</div> */}
+            <div className={styles.backgroundImage}><img src={currentMovie[0].coverUrl} alt="" /></div>
+            <div className={styles.year}>{currentMovie[0].year}</div>
             {/* <div className={styles.genres}>
                 {
-                    genres.map((g) => <div>{g}</div>)
+                    currentMovie[0].genres.map((g) => <div>{g}</div>)
                 }
             </div> */}
-            {/* <div>{description}</div> */}
-            <div>Инфо о фильме id:{id}</div>
+            <div className={styles.description}>{currentMovie[0].description}</div>
         </div>
 
     )
