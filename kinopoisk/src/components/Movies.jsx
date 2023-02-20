d39aebd08a9dab104b4e046d62ce2f9c3835e3a2
ksenias <kseniatsn@gmail.com>
2023-02-20 20:39:41 +0300

    fantasy filter

--- kinopoisk/src/components/Movies.jsx
+++ kinopoisk/src/components/Movies.jsx
@@ -1,23 +1,119 @@
 import styles from './../styles/Movies.module.css';
+import { IoIosArrowDown } from 'react-icons/io';
 import PageTitle from './PageTitle';
+import { useState } from 'react';
 import Filters from './Filters';
 import Card from './Card.jsx'
 
+
+
 function Movies({ movies }) {
-    console.log(movies)
+
+    const [pressed, setPressed] = useState(false);
+    const [fantasyPressed, fantasySetPressed] = useState(false);
+    const [array, setMovies] = useState([])
+
+    const OpenFilters = () => {
+        setPressed(!pressed)
+    }
+
+    const ShowFantasy = () => {
+        fantasySetPressed(!fantasyPressed);
+        let fantasyArray = Object.assign([], array);
+
+        movies.map((movie) =>
+            movie.genres.find((element) => {
+                if (element.genre == 'фэнтези') {
+                    fantasyArray.push(movie)
+                }
+            }
+            ))
+        setMovies(fantasyArray)
+    }
+
+    // const ShowHistorycal = () => {
+    //     fantasySetPressed(!fantasyPressed);
+    //     let historycalArray = Object.assign([], array);
+
+    //     movies.map((movie) =>
+    //         movie.genres.find((element) => {
+    //             if (element.genre == 'история') {
+    //                 historycalArray.push(movie)
+    //             }
+    //         }
+    //         ))
+    //     setMovies(historycalArray)
+    // }
+
     return (
         <>
             <PageTitle />
-            <Filters />
-            <div className={styles.container}>
+            <section className={styles.filtersContainer}>
+                <section className={styles.filtersLeftSection}>
+                    <button className={styles.filterButton} onClick={OpenFilters}>Жанры<IoIosArrowDown /></button>
+                    {
+                        pressed &&
+                        <section className={styles.filtersBlock}>
+                            <input type='button' value='Драма' />
+                            <input type='button' value='Фентези' onClick={ShowFantasy} />
+                            <input type='button' value='Военный' />
+                            <input type='button' value='История'
+                            // onClick={ShowHistorycal} 
+                            />
+                            <input type='button' value='Приключения' />
+                        </section>
+                    }
+                    <button className={styles.filterButton}>Рейтинг<IoIosArrowDown /></button>
+                    <button className={styles.filterButton}>Годы выхода<IoIosArrowDown /></button>
+                </section>
+                <section className={styles.filtersRightSection}>
+                    <div>Бесплатно</div>
+                    <label className={styles.switch}>
+                        <input type="checkbox" className={styles.checkbox} />
+                        <span className={styles.slider}></span>
+                    </label>
+                    <button className={styles.filterButton}>Рекомендуемые<IoIosArrowDown /></button>
+                </section>
+            </section>
+            <div className={styles.moviesContainer}>
+
                 {
-                    movies.map((movie) =>
+                    fantasyPressed ?
+                        <>
+                            {
+                                array.map((movie) =>
+                                    <Card
+                                        image={movie.posterUrl}
+                                        id={movie.filmId}
+                                    />
+                                )
+                            }
+                        </> :
+                        <>
+                            {
+                                movies.map((movie) =>
+                                    <Card
+                                        image={movie.posterUrl}
+                                        id={movie.filmId}
+                                    />
+                                )
+                            }
+                        </>
+
+                }
+
+
+                {/* {
+                    array.map((movie) =>
                         <Card
                             image={movie.posterUrl}
                             id={movie.filmId}
                         />
                     )
-                }
+           
+              } */}
+
+
             </div>
         </>
     )
